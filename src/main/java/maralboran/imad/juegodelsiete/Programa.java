3d13cdfc5e877193e0d5e55f6f35b1da5e760f1d
stringimad <imadelfahssi@gmail.com>
2021-11-03 14:09:54 +0100

    Crea el programa y la clase main

---
+++ src/main/java/maralboran/imad/juegodelsiete/Programa.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package maralboran.imad.juegodelsiete;
+
+/**
+ *
+ * @author imad
+ */
+public class Programa {
+    
+}
